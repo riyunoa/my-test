b84ca20b704c51c2f683290b667a7b948d0fb9a5
Gillian Ng <gillian@rockinfo.com.au>
2017-09-20 17:07:54 +1000

    test

---
+++ lib/entity-picker.module.ts
@@ -0,0 +1,24 @@
+import {NgModule} from '@angular/core';
+import {HttpModule, JsonpModule} from '@angular/http';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+
+@NgModule({
+    imports:      [
+        CommonModule,
+        HttpModule,
+        JsonpModule,
+        FormsModule
+    ],
+    declarations: [
+
+    ],
+    exports:      [
+    ],
+    providers:    [
+    ]
+})
+
+export class EntityPickerModule {
+
+}
